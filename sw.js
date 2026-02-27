a2f6a96e5f71f0619ab3088b25ae62e05b7a933a
aqplemine <leventbas14@gmail.com>
2026-02-26 21:15:32 -0800

    Create sw.js
    a

---
+++ sw.js
@@ -0,0 +1,8 @@
+//THIS IS NOT USED FOR MY PROFILE!
+//WHEN I MAKE PROJECTS THAT USE SW.JS, THEY COME HERE A LOT, SO I WANNA INSTALL IT!
+//YEA
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", () => {});
